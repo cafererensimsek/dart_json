2206243a44bbf1fadae3fb18423ff2cd63b74c47
Eren Şimşek <scafereren@gmail.com>
2020-08-05 20:13:09 +0300

    commit

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cash_coach/filter_internals.dart';
 import 'package:cash_coach/get_transfers.dart';
 import 'package:cash_coach/transaction.dart';
@@ -7,5 +9,6 @@ void main() async {
   Map<String, List<Transaction>> internals =
       filterInternals(results[0], results[1]);
 
+  sleep(const Duration(minutes: 1));
   print(internals);
 }
